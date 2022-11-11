19c680de1d8cb10a0522f81e9fe9497803fb4bcb
Nma Shawkat <nmashawket@gmail.com>
2022-11-11 12:47:31 +0300

    done with this

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    cats.unshift("Boby");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+    const appendCat= [...cats, "Broom"];
+    return appendCat;
+}
+function prependCat() {
+    const appendCat= ["Arnold", ...cats];
+    return appendCat;
+}
+function removeLastCat() {
+    const removeLastCat= cats.slice(0, -1);
+    return removeLastCat;
+}
+
+function removeFirstCat() {
+    const removeFirstCat= cats.slice(1);
+    return removeFirstCat;
+}
